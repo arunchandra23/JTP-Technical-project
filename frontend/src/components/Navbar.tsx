d29e364c2f731d48ee4a27ea85373107027c5f3f
arunchandra23 <arunboini999@gmail.com>
2024-04-08 21:57:22 +0530

    FE:changed nabar text

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
       <nav className="bg-indigo-700 text-white p-6 sticky top-0 left-0 w-full z-10">
         <div className="container mx-auto flex justify-center">
           <h1 className="text-2xl font-bold">
-            Product Recommendation System
+            Image Recommendation System
           </h1>
         </div>
       </nav>
