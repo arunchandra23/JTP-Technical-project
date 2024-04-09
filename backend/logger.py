962b118652b024de4e1793da75fbb60329cede2a
arunchandra23 <arunboini999@gmail.com>
2024-04-09 15:22:08 +0530

    added notebooks for preprocessing and training

--- backend/logger.py
+++ backend/logger.py
@@ -2,11 +2,6 @@ import logging
 import os
 from dotenv import load_dotenv
 load_dotenv()
-# Disable uvicorn access logger
-# uvicorn_access = logging.getLogger("uvicorn.access")
-# uvicorn_access.disabled = True
-# logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-#     datefmt='%Y-%m-%d:%H:%M:%S')
 
 logger = logging.getLogger("app")
-logger.setLevel(logging.getLevelName(os.getenv('LOGGING_LEVEL')))
+logger.setLevel(logging.getLevelName(os.getenv('LOGGING_LEVEL')==None and 'INFO' or os.getenv('LOGGING_LEVEL')))
